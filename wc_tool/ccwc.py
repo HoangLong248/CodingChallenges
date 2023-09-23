e36959df1c8aa822720f1dbc02da027c5160793a
Long Tran <tranhuuhoanglong248@gmail.com>
2023-09-23 10:45:26 +0000

    Finished step three

--- wc_tool/ccwc.py
+++ wc_tool/ccwc.py
@@ -16,15 +16,24 @@ def get_lines_of_file(fin):
         lines = f.readlines()
     return len(lines)
 
+def get_words_of_files(fin):
+    with open(fin) as f:
+        words = f.read().split()
+    return len(words)
+
 @click.command("ccwc")
 @click.option("-c", "--bytes", is_flag=True, help="print the byte counts", default=False)
 @click.option("-l", "--lines", is_flag=True, help="print the newline counts", default=False)
+@click.option("-w", "--words", is_flag=True, help="print the word counts")
 @click.argument('filename', type=click.Path(exists=True))
-def ccwc(bytes, lines, filename):
+def ccwc(bytes, lines, words, filename):
     """ccwc - print newline, word, and byte counts for each file"""
     if bytes:
-        c_bytes = get_size_of_file(filename)
-        print(c_bytes, filename)
+        num_bytes = get_size_of_file(filename)
+        print(num_bytes, filename)
     if lines:
-        l_lines = get_lines_of_file(filename)
-        print(l_lines, filename)
+        num_lines = get_lines_of_file(filename)
+        print(num_lines, filename)
+    if words:
+        num_words = get_words_of_files(filename)
+        print(num_words, filename)
