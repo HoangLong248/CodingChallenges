e4012da6b09d2d3c33180cdca0c93d62ded8505c
Long Tran <tranhuuhoanglong248@gmail.com>
2023-09-23 12:07:58 +0000

    Finished Project WC Tool

--- wc_tool/ccwc.py
+++ wc_tool/ccwc.py
@@ -1,39 +1,57 @@
 import click
 import os
-
-def read_file(fin):
-    content = None
-    with open(fin) as f_in:
-        content = f_in.read()
-    return content
+import sys
 
 def get_size_of_file(fin):
-    size = os.path.getsize(fin)
-    return size
+    n_size = len((bytes(fin, 'utf-8')))
+    if n_size:
+        return n_size
+    return 0
 
 def get_lines_of_file(fin):
-    with open(fin) as f:
-        lines = f.readlines()
-    return len(lines)
+    n_lines = fin.count('\n')
+    return n_lines
 
 def get_words_of_files(fin):
-    with open(fin) as f:
-        words = f.read().split()
-    return len(words)
+    n_words = fin.split()
+    return len(n_words)
+
+def get_char_of_files(fin):
+    return len(fin)
 
 @click.command("ccwc")
 @click.option("-c", "--bytes", is_flag=True, help="print the byte counts", default=False)
 @click.option("-l", "--lines", is_flag=True, help="print the newline counts", default=False)
-@click.option("-w", "--words", is_flag=True, help="print the word counts")
-@click.argument('filename', type=click.Path(exists=True))
-def ccwc(bytes, lines, words, filename):
+@click.option("-w", "--words", is_flag=True, help="print the word counts", default=False)
+@click.option("-m", "--chars", is_flag=True, help="print the characters counts", default=False)
+@click.argument('file', type=click.File(), default=sys.stdin)
+def ccwc(bytes, lines, words, chars, file):
     """ccwc - print newline, word, and byte counts for each file"""
-    if bytes:
-        num_bytes = get_size_of_file(filename)
-        print(num_bytes, filename)
-    if lines:
-        num_lines = get_lines_of_file(filename)
-        print(num_lines, filename)
-    if words:
-        num_words = get_words_of_files(filename)
-        print(num_words, filename)
+    output = []
+    console_out = ""
+    filename = file.name
+    fin = file.read()
+    if (bytes == False) and (lines == False) and (words == False) and (chars == False):
+        output.append(get_size_of_file(fin))
+        output.append(get_lines_of_file(fin))
+        output.append(get_words_of_files(fin))
+    else:
+        if bytes:
+            num_bytes = get_size_of_file(fin)
+            output.append(num_bytes)
+        if lines:
+            num_lines = get_lines_of_file(fin)
+            output.append(num_lines)
+        if words:
+            num_words = get_words_of_files(fin)
+            output.append(num_words)
+        if chars:
+            num_chars = get_char_of_files(fin)
+            output.append(num_chars)
+    
+        
+    for item in sorted(output):
+        console_out += f"{item} "
+    if file != sys.stdin:
+        console_out += f"{filename}"
+    print(console_out)
