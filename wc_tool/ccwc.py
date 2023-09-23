94be94341d3d2e1b13c806625e88655a5383393f
Long Tran <tranhuuhoanglong248@gmail.com>
2023-09-23 10:21:15 +0000

    Finshed Step One

--- wc_tool/ccwc.py
+++ wc_tool/ccwc.py
@@ -1,9 +1,20 @@
 import click
+import os
+
+def read_file(fin):
+    content = None
+    with open(fin) as f_in:
+        content = f_in.read()
+    return content
+
+def get_size_of_file(fin):
+    size = os.path.getsize(fin)
+    return size
 
 @click.command("ccwc")
 @click.option("-c", "--bytes", is_flag=True, help="print the byte counts", default=False)
-@click.argument('filename', type=click.File(), default="-", required=False)
+@click.argument('filename', type=click.Path(exists=True))
 def ccwc(bytes, filename):
     """ccwc - print newline, word, and byte counts for each file"""
-    click.echo("Hello World")
-    click.echo(file=filename)
+    if bytes:
+        print(get_size_of_file(filename))
